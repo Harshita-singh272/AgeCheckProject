e10223173570ca2f2eae61a5ecee68d94fc69def
Harshita-singh272 <harshitasinghixa@gmail.com>
2026-07-01 20:06:09 +0530

    Completed Phase 2 awaiting verification

--- demo/streamlit.py
+++ demo/streamlit.py
@@ -101,7 +101,7 @@ h1{
     font-size: 35px !important;  
     padding-left: 5px !important;
     padding-top: 10px !important;
-    margin-bottom: 0.5rem !important; 
+    margin-bottom: 0.2rem !important; 
     line-height:1.2;    
 } 
             
@@ -122,22 +122,100 @@ hr{
 
 .privacy-box {
     margin-top: -10px !important;
+    margin-bottom: .7rem;
     padding: 12px;
     border-radius: 8px;
     background-color: #f5f7fb;
     border: 1px solid #dbe3f0;
-    font-size: 16px;
+    font-size: 18px;
 }         
+
+.about {
+    font-size: 27px;
+    color: #003049 !important;           
+    font-weight: 500;
+}
+    
+.about-data {
+    font-size: 17px;
+    color: #003049 !important;   
+    margin-bottom: 1.5rem;      
+    font-weight: 400;        
+}
+            
+.left_description {
+    font-size: 18px;
+    margin-top: -6px;
+    margin-bottom:10px;
+    color: #343a40 !important;            
+}
+            
+.caption {
+    margin-top: -8px;
+    color: #adb5bd;
+    text-align: center;
+    font-size: 15px;
+}
+            
+.uploaded {
+    font-weight: 600;   
+    font-size: 19px;         
+}
+
+.decision-card{
+    padding:20px;
+    padding-top: 10px !important;
+    border-radius:12px;
+    text-align:left;
+    min-height:90px;
+    margin-bottom: .5rem;
+}
+
+.pass{
+    border:2px solid #22C55E;
+    background:#F0FDF4;
+    padding-right:10px;
+}
+
+.inconclusive{
+    border:2px solid #F59E0B;
+    background:#FFFBEB;
+}
+
+.fail{
+    border:2px solid #EF4444;
+    background:#FEF2F2;
+    padding-right:10px !important;
+}
+
+.icon{
+    font-size:20px;
+}
+
+.title{
+    font-size:20px;
+    font-weight:500;
+    margin-top:5px;
+}
+
+.desc{
+    font-size:15px;
+    margin-top:5px;
+    color:#4B5563;
+}           
 </style>
 """, unsafe_allow_html=True)
 st.title("Age Check with Boolean Privacy Demo")
-left , centre , right = st.columns([5,6,5])
+left , centre , right = st.columns([5,7,4])
 
 with left:
     container = st.container(border=True)
 
     with container:
         st.subheader("⚙ Configuration")
+        st.markdown(
+            '<div class="left_description">[Configure age verification settings]</div>',unsafe_allow_html=True
+        )
         st.divider()
         st.markdown(
             '<div class="upload-title">1. Upload Image</div>',
@@ -151,18 +229,18 @@ with left:
         
         if uploaded_file is not None:
             st.info("Upload area")
-            st.image(uploaded_file)
+            st.image(uploaded_file , width=90)
        
         st.markdown(
             '<div class="threshold-title">2. Select Age Threshold</div>',
             unsafe_allow_html=True
         )
         threshold = st.radio(
-            "Choose the minimum age that a user must meet to pass age verification.",
+            "💡 Choose the minimum age that a user must meet to pass age verification.",
             ["18+ (Default)","21+","25+","60+"]
         )
 
-        st.button("🛡️ VERIFY AGE" , use_container_width=True)
+        verification = st.button("🛡️ VERIFY AGE" , use_container_width=True)
         st.markdown(
             '</div>',
             unsafe_allow_html=True
@@ -174,15 +252,207 @@ with left:
             </div>
         """, unsafe_allow_html=True)
 
+    container = st.container(border=True)
+
+    with container:
+        st.markdown(
+            '<div class="about">📋 About this System</div>',
+            unsafe_allow_html=True)
+        
+        st.divider()
+
+        st.markdown(
+            '<div class=about-data>This demo verifies whether a user is above a selected age threshold without revealing the exact age. It follows the principle of data minimization and privacy by design.</div>',
+        unsafe_allow_html=True)
+
 with centre:
     container = st.container(border=True)
     with container:
         st.subheader("User Panel")
+        if verification == False:
+            st.markdown(
+            '<div class="uploaded">Uploaded Image</div>',
+            unsafe_allow_html=True
+            ) 
+            col1, col2, col3 = st.columns([2,2, 2])
+
+            with col2:
+                container = st.container(border=True)
+
+                with container:
+                        # col1, col2, col3 = st.columns([0.1,2, 0.2])
+                        # with col2:
+                        st.image('data/test_images/profile.png' , width = 250)
+                        st.markdown(
+                            '<div class=caption>No Image Uploaded</div>',unsafe_allow_html=True
+                        )            
+            st.markdown(
+            """<div class="uploaded">Verification Result</div>""",
+            unsafe_allow_html=True
+            )
+            placeholder = st.container(border=True)
+
+            with placeholder:
+
+                left_icon, right_text = st.columns([1, 8])
+
+                with left_icon:
+                    st.markdown(
+                        '<div style="color:#014f86;font-size:40px;text-align:center;margin-top: -.7rem;">ⓘ</div>',
+                        unsafe_allow_html=True
+                    )
+
+                with right_text:
+                    st.markdown(
+                        """
+                        <div style="color:#014f86; font-size:32px; font-weight:600; margin-bottom:0; margin-top: -0.5rem;">
+                            Awaiting Verification
+                        </div>
+                        """,
+                    unsafe_allow_html=True
+                    )
+
+                    st.markdown(
+                         """
+                        <div style="
+                            color:#335c67; margin-bottom: .5rem;
+                          ">
+                        "Upload an image and click **VERIFY AGE** to see the result."
+                        </div>
+                        """,
+                    unsafe_allow_html=True
+                    )
+            st.markdown(
+            """<div class="uploaded">Confidence Level</div>""",
+            unsafe_allow_html=True
+            )
+            # Top row
+            col1, col2 = st.columns([1, 3])
+
+            with col1:
+                st.markdown("""
+                <div style="
+                    font-size:40px;
+                    font-weight:700;
+                    color:#0f172a;
+                ">
+                    0%
+                </div>
+                """, unsafe_allow_html=True)
+
+            with col2:
+                st.markdown("""
+                <div style="
+                    text-align:right;
+                    margin-top:15px;
+                    color:#64748b;
+                    font-size:15px;
+                ">
+                    No data yet
+                </div>
+                """, unsafe_allow_html=True)
+
+            # Progress Bar
+            st.markdown("""
+            <div style="
+                width:100%;
+                height:12px;
+                background:#e5e7eb;
+                border-radius:10px;
+                margin-top:5px;
+                margin-bottom:10px;
+            "></div>
+            """, unsafe_allow_html=True)
+
+            # Scale
+            scale1, scale2, scale3, scale4, scale5 = st.columns(5)
+
+            with scale1:
+                st.markdown("<div style='text-align:left;'>0%</div>", unsafe_allow_html=True)
+
+            with scale2:
+                st.markdown("<div style='text-align:center;'>25%</div>", unsafe_allow_html=True)
+
+            with scale3:
+                st.markdown("<div style='text-align:center;'>50%</div>", unsafe_allow_html=True)
+
+            with scale4:
+                st.markdown("<div style='text-align:center;'>75%</div>", unsafe_allow_html=True)
+
+            with scale5:
+                st.markdown("<div style='text-align:right;'>100%</div>", unsafe_allow_html=True)
+
+            st.markdown("<br>", unsafe_allow_html=True)
+
+            # Legend
+            legend1, legend2, legend3 = st.columns(3)
+
+            with legend1:
+                st.markdown(
+                    "<div style='text-align:center;color:#475569;margin-bottom:8px'>🔴 0–59% &nbsp; Low</div>",
+                    unsafe_allow_html=True
+                )
+
+            with legend2:
+                st.markdown(
+                    "<div style='text-align:center;color:#475569;'>🟡 60–79% &nbsp; Medium</div>",
+                    unsafe_allow_html=True
+                )
+
+            with legend3:
+                st.markdown(
+                    "<div style='text-align:center;color:#475569;'>🟢 80–100% &nbsp; High</div>",
+                    unsafe_allow_html=True
+                )     
+            st.info("ⓘ &nbsp; Only a boolean result is shown in this view to protect user privacy.")  
+        else: 
+            st.write("hello")    
+    Decision = st.container(border=  True)
+
+    with Decision:
+        st.markdown(
+        '<div class="about">Decision Type</div>',
+        unsafe_allow_html=True)
+        st.divider()
+        col1 , col2 ,col3 = st.columns([1,1.7,1])
+
+        with col1:
+            st.markdown("""
+            <div class="decision-card pass">
+                <div class="title" style="color:#16A34A;">✅  PASS</div>
+                <div class="desc">
+                    Age is above<br>
+                    threshold
+                </div>
+            </div>
+            """, unsafe_allow_html=True)
+
+        with col2:
+            st.markdown("""
+            <div class="decision-card inconclusive">
+                <div class="title" style="color:#F59E0B;">⚠️ INCONCLUSIVE</div>
+                <div class="desc">
+                    Prediction is close to threshold
+                    or confidence is low
+                </div>
+            </div>
+            """, unsafe_allow_html=True)
+
+        with col3:
+            st.markdown("""
+            <div class="decision-card fail">
+                <div class="title" style="color:#DC2626;">❌  FAIL</div>
+                <div class="desc">
+                    Age is below<br>
+                    threshold
+                </div>
+            </div>
+            """, unsafe_allow_html=True)
+
 
 with right:
-    st.markdown("""
-    <style>
-    
-    </style>
-    """ , unsafe_allow_html = True)
-    st.subheader("Admin Access")        
+
+
+    container = st.container(border=True)
+    with container:
+        st.subheader("Admin Access")        
